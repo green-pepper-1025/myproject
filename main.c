3bf74ff7c77e58f4ccf57a186ea5b5e6ac9e9cd5
green <green_1025@qq.com>
2025-11-22 21:28:52 +0800

    remote commit

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
     printf("Hello World\n");
-    printf("First change\n");
-    printf("Change on here\n");
-    printf("Second change\n");
+    printf("First change had been changed\n");
+    //Second change had been deleted
     printf("Third change\n");
     printf("Added to New branch\n");
     return 0;
