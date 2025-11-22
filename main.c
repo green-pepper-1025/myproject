896e198ee3c94633ba23d12ff90fdeb6a3825618
green <green_1025@qq.com>
2025-11-22 22:08:51 +0800

    new

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 int main() {
     printf("Hello World\n");
-<<<<<<< HEAD
     printf("First change\n");
     printf("New Change on here\n");
-    printf("Second change\n");
-=======
-    printf("First change had been changed\n");
     //Second change had been deleted
->>>>>>> 3bf74ff7c77e58f4ccf57a186ea5b5e6ac9e9cd5
     printf("Third change\n");
     printf("Added to New branch\n");
     return 0;
