ce683c847383cabfe887ff9c0fc2fe0760ee76a5
green <green_1025@qq.com>
2025-11-22 21:45:24 +0800

    local commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 int main() {
     printf("Hello World\n");
     printf("First change\n");
-    printf("Change on here\n");
+    printf("New Change on here\n");
     printf("Second change\n");
     printf("Third change\n");
     printf("Added to New branch\n");
